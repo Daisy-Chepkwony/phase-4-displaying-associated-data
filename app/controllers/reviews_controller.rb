80165b1b02b9ba7d33d0a9714b27d61ec6d84fc4
+ <+>
2023-03-21 11:34:07 +0300

    finished assignment

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,8 +1,12 @@
 class ReviewsController < ApplicationController
 
+  # def index
+  #   reviews = Review.all.order(rating: :desc)
+  #   render json: reviews
+  # end
   def index
     reviews = Review.all.order(rating: :desc)
-    render json: reviews
+    render json: reviews, include: :dog_house
   end
 
 end
